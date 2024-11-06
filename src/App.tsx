01e3181ce581e57dc3f05998adee22ed6f1e41c7
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-06 20:17:07 +0800

    feat: add new components to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { useState } from 'react';
+import { ThemeContext } from './store/themeContext';
+import { Navigation, Layout, Logo, SidePanel, MainPanel } from './components';
 
+import styles from './App.module.scss';
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(prevMode => !prevMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      <div className={styles.app} data-theme={isDarkMode ? 'dark' : 'light'}>
+        <Layout>
+          <SidePanel>
+            <Logo />
+            <div className="divider" />
+            <Navigation />
+            <div className="divider" />
+          </SidePanel>
+          <MainPanel>
+            <h1>Main Panel</h1>
+          </MainPanel>
+        </Layout>
+      </div>
+    </ThemeContext.Provider>
+
   );
 }
 
