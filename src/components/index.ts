aa2dc22de6313553907265f5c342998539724fd5
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-06 20:16:28 +0800

    feat: add explicit export and import of all the components

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Layout from "./Layout/Layout";
+import Logo from "./Logo/Logo";
+import MainPanel from "./MainPanel/MainPanel";
+import Navigation from "./Navigation/Navigation";
+import SidePanel from "./SidePanel/SidePanel";
+
+export { Layout, Logo, MainPanel, Navigation, SidePanel };
