ff685965f2f13022e4d5bf2ae97f5ebcdab58e6d
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-07 11:22:12 +0800

    feat: add dynamic labels to the toggle switch component
    - Cover the test case

--- src/components/ToggleSwitch/ToggleSwitch.test.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.test.tsx
@@ -1,10 +1,11 @@
 import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import ToggleSwitch from './ToggleSwitch';
 
 describe('ToggleSwitch Component', () => {
   const setup = (isChecked: boolean) => {
     const onToggle = jest.fn();
-    const utils = render(<ToggleSwitch id="test-toggle" isChecked={isChecked} onToggle={onToggle} />);
+    const utils = render(<ToggleSwitch labels={['Left', 'Right']} id="test-toggle" isChecked={isChecked} onToggle={onToggle} />);
     const checkbox = screen.getByRole('checkbox') as HTMLInputElement;
     return {
       checkbox,
@@ -37,11 +38,17 @@ describe('ToggleSwitch Component', () => {
 
   test('renders Dark label when checked', () => {
     const { getByText } = setup(true);
-    expect(getByText('Dark')).toHaveClass('selected');
+    expect(getByText('Left')).toHaveClass('selected');
   });
 
   test('renders Light label when unchecked', () => {
     const { getByText } = setup(false);
-    expect(getByText('Light')).toHaveClass('selected');
+    expect(getByText('Right')).toHaveClass('selected');
+  });
+
+  test('displays correct labels', () => {
+    const { getByText } = setup(true);
+    expect(getByText('Left')).toBeInTheDocument();
+    expect(getByText('Right')).toBeInTheDocument();
   });
 });
--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -5,20 +5,23 @@ interface ToggleSwitchProps {
   id: string;
   isChecked: boolean;
   onToggle: (checked: boolean) => void;
+  labels: [string, string];
 }
 
 const ToggleSwitch: React.FC<ToggleSwitchProps> = ({
   id,
   onToggle,
   isChecked,
+  labels,
 }) => {
+  const [onText, offText] = labels;
 
   return (
     <div className={styles.container}>
       <input id={id} type="checkbox" checked={isChecked} onChange={(e) => onToggle(e.target.checked)} className={styles.checkbox} name="test" />
       <label className={styles.label} htmlFor={id}>
-        <span className={`${styles.switch} ${isChecked ? styles.selected : ''}`}>Dark</span>
-        <span className={`${styles.switch} ${!isChecked ? styles.selected : ''}`}>Light</span>
+        <span className={`${styles.switch} ${isChecked ? styles.selected : ''}`}>{onText}</span>
+        <span className={`${styles.switch} ${!isChecked ? styles.selected : ''}`}>{offText}</span>
       </label>
     </div>
   );
