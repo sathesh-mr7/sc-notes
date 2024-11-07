7dfea78bb1e1c7185a697651c073868e2fcfe47d
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-07 21:51:59 +0800

    feat: add edge props to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,9 +5,15 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   className?: string;
   variant?: 'primary' | 'secondary' | 'tertiary';
+  corner?: 'default' | 'edged';
 }
-const Button: React.FC<ButtonProps> = ({ children, variant = 'primary', className, ...props }) => {
-  return <button className={`${styles.button} ${styles[variant]} ${className ?? ''}`} {...props}>{children}</button>
+const Button: React.FC<ButtonProps> = ({ children, variant = 'primary', corner, className, ...props }) => {
+  return (
+    <button
+      className={`${styles.button} ${variant ? styles[variant] : ''} ${className ?? ''} ${corner ? styles[corner] : ''}`}
+      {...props}>
+      {children}
+    </button>)
 }
 
 export default Button;
