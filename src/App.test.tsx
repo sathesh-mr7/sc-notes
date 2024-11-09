8ac3e7771a3491e707bcf6603d6e7b6122657a06
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-09 23:27:28 +0800

    feat: add new component to app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,12 @@
 import { render } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store/reduxStore';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
 import { useState } from 'react';
-import { ThemeContext } from './store/themeContext';
-import { Navigation, Layout, Logo, SidePanel, MainPanel } from './components';
+import { useDispatch } from 'react-redux';
+import { ThemeProvider } from './store/themeContext';
+import { showModal } from './store/modalSlice';
+
+import { Layout, Logo, MainPanel, Navigation, SidePanel, ToggleSwitch } from './components';
+import AddNotesButton from './components/AddNotesButton/AddNotesButton';
+import Breadcrumbs from './components/Breadcrumbs/Breadcrumbs';
+import FilterBar from './components/FilterBar/FilterBar';
+import Notes from './components/Notes/Notes';
+import PortalRoot from './components/PortalRoot/PortalRoot';
+import SearchNotes from './components/SearchNotes/SearchNotes';
 
 import styles from './App.module.scss';
+import { NOTE_MODAL_ID } from './constants';
+
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(() => localStorage.getItem('isDarkMode') === 'true');
+  const dispatch = useDispatch();
 
   const toggleDarkMode = () => {
+    localStorage.setItem('isDarkMode', JSON.stringify(!isDarkMode));
     setIsDarkMode(prevMode => !prevMode);
   };
 
   return (
-    <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
-      <div className={styles.app} data-theme={isDarkMode ? 'dark' : 'light'}>
+    <ThemeProvider value={{ isDarkMode, toggleDarkMode }}>
+      <div className={styles.container} data-theme={isDarkMode ? 'dark' : 'light'}>
+        <PortalRoot />
         <Layout>
           <SidePanel>
             <Logo />
@@ -21,12 +35,29 @@ function App() {
             <div className="divider" />
           </SidePanel>
           <MainPanel>
-            <h1>Main Panel</h1>
+            <section className={`${styles.section} ${styles.topSection}`}>
+              <Breadcrumbs />
+              <ToggleSwitch labels={['dark', 'light']} id="mode" onToggle={toggleDarkMode} isChecked={isDarkMode} />
+            </section>
+            <section className={styles.section}>
+              <AddNotesButton onClick={() => dispatch(showModal(NOTE_MODAL_ID))} />
+              <SearchNotes onChange={(text) => console.log(text)} />
+            </section>
+            <section className={`${styles.filterBar} ${styles.section}`}>
+              <FilterBar />
+            </section>
+            <section className={`${styles.filterBar} ${styles.section}`}>
+              <Notes />
+            </section>
           </MainPanel>
         </Layout>
+        <footer className={styles.footer}>
+          <div className={styles.wrapper}>
+            <p>© 2024 Sathesh Kumar</p>
+          </div>
+        </footer>
       </div>
-    </ThemeContext.Provider>
-
+    </ThemeProvider>
   );
 }
 
