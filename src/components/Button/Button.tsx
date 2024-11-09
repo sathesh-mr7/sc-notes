42273cf8b39e1f4665c44ba7c715dcc09fd8c6aa
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-09 11:51:15 +0800

    feat: add size to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,14 @@ import styles from './Button.module.scss';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   className?: string;
+  size?: 'sm' | 'md';
   variant?: 'primary' | 'secondary' | 'tertiary';
   corner?: 'default' | 'edged';
 }
-const Button: React.FC<ButtonProps> = ({ children, variant = 'primary', corner, className, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ children, variant = 'primary', corner, size = 'md', className, ...props }) => {
   return (
     <button
-      className={`${styles.button} ${variant ? styles[variant] : ''} ${className ?? ''} ${corner ? styles[corner] : ''}`}
+      className={`${styles.button} ${styles[size]} ${styles[variant]} ${className ?? ''} ${corner ? styles[corner] : ''}`}
       {...props}>
       {children}
     </button>)
