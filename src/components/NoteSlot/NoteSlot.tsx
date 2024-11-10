f7d984e94ba00602feeb43b643484541f30592b7
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-10 20:05:27 +0800

    feat: add readonly props to noteSlot to unitilize this component in trash

--- src/components/NoteSlot/NoteSlot.tsx
+++ src/components/NoteSlot/NoteSlot.tsx
@@ -6,16 +6,16 @@ import ActionMenu from '../ActionMenu/ActionMenu';
 import { ReactComponent as TrashIcon } from '../../assets/icons/trash.svg';
 import { ReactComponent as EllipsisIcon } from "../../assets/icons/ellipsis-vertical.svg";
 
-
 import styles from './NoteSlot.module.scss';
 
 interface NoteSlotProps {
   className?: string;
   note: Note;
   onNoteClick: (note: Note) => void;
+  readonly?: boolean;
 }
 
-const NoteSlot: React.FC<NoteSlotProps> = ({ note, onNoteClick, className }) => {
+const NoteSlot: React.FC<NoteSlotProps> = ({ note, onNoteClick, className, readonly }) => {
   const [showActionMenu, setShowActionMenu] = React.useState(false);
   const handleAction = (event: React.MouseEvent<HTMLLIElement, MouseEvent>, action: string) => {
     event.stopPropagation();
@@ -32,10 +32,14 @@ const NoteSlot: React.FC<NoteSlotProps> = ({ note, onNoteClick, className }) => 
         <div className={styles.tags}>
           {note.tag ? <Tag color={note.tag.color} text={note.tag.text} /> : null}
         </div>
-        <div className={styles.actionMenu}>
-          <span className={styles.icon} onClick={handleOnActionMenuClick}><EllipsisIcon className={styles.ellipsisIcon} /></span>
-          <ActionMenu show={showActionMenu} actions={['Edit', 'Delete']} onClick={handleAction} className={styles.actionMenuList} />
-        </div>
+
+        {!readonly ? (
+          <div className={styles.actionMenu}>
+            <span className={styles.icon} onClick={handleOnActionMenuClick}><EllipsisIcon className={styles.ellipsisIcon} /></span>
+            <ActionMenu show={showActionMenu} actions={['Edit', 'Delete']} onClick={handleAction} className={styles.actionMenuList} />
+          </div>
+        ) : null}
+
       </header>
       <div className={styles.content}>
         <h3 className={styles.title}>{note.title}</h3>
@@ -44,9 +48,11 @@ const NoteSlot: React.FC<NoteSlotProps> = ({ note, onNoteClick, className }) => 
       <div className='divider' />
       <footer className={styles.footer}>
         <span className={styles.createdDate}>{note.createdAt}</span>
-        <DeleteNote note={note} onDelete={onNoteClick}>
-          <TrashIcon className={styles.deleteIcon} />
-        </DeleteNote>
+        {!readonly ? (
+          <DeleteNote note={note} onDelete={onNoteClick}>
+            <TrashIcon className={styles.deleteIcon} />
+          </DeleteNote>
+        ) : null}
       </footer>
     </div>
   )
