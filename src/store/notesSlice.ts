cb9c2c0b0ddef8cc4f5116fa1ab2430308763941
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-10 15:03:49 +0800

    feat: get notes from localstorage db

--- src/store/notesSlice.ts
+++ src/store/notesSlice.ts
@@ -1,97 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getNotes, setNotes } from "../database/localstorage";
 
-const initialState: Array<Note> = [
-  {
-    id: '1',
-    title: "First note",
-    content: "This is the first note",
-    createdAt: 'Mar 1, 2024',
-    updatedAt: 'Mar 1, 2024',
-  },
-  {
-    id: '2',
-    title: "Second note",
-    content: "This is the second note",
-    createdAt: 'Mar 2, 2024',
-    updatedAt: 'Mar 2, 2024',
-    parent: 'P1',
-  },
-  {
-    id: '3',
-    title: "Third note",
-    content: "This is the third note",
-    createdAt: 'Mar 3, 2024',
-    updatedAt: 'Mar 3, 2024',
-    tag: {
-      text: 'Personal',
-      color: 'blue'
-    }
-  },
-  {
-    id: '4',
-    title: "Fourth note",
-    content: "This is the fourth note",
-    createdAt: 'Mar 4, 2024',
-    updatedAt: 'Mar 4, 2024',
-    tag: {
-      text: 'Personal',
-      color: 'blue'
-    },
-    parent: 'P1'
-  },
-  {
-    id: '5',
-    title: "Fifth note",
-    content: "This is the fifth note",
-    createdAt: 'Mar 5, 2024',
-    updatedAt: 'Mar 5, 2024',
-    tag: {
-      text: 'Work',
-      color: 'green'
-    }
-  },
-  {
-    title: "First Note",
-    content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-    tag: {
-      text: "work",
-      color: "blue",
-    },
-    id: "11",
-    parent: null,
-    createdAt: 'Mar 12, 2024',
-    updatedAt: 'Mar 12, 2024',
-  },
-  {
-    title: "Second Note",
-    content: "This is the content of the second note.",
-    tag: {
-      text: "personal",
-      color: "green",
-    },
-    id: "12",
-    parent: "P3",
-    createdAt: 'Mar 12, 2024',
-    updatedAt: 'Mar 12, 2024',
-  },
-  {
-    id: "13",
-    title: "Third Note",
-    content: "This is the content of the third note.",
-    createdAt: 'Mar 12, 2024',
-    updatedAt: 'Mar 12, 2024',
-  },
-];
+const initialState: Array<Note> = getNotes();
 
 const notesSlice = createSlice({
   name: 'notes',
   initialState,
   reducers: {
     addNote: (state, action) => {
+
       state.push(action.payload);
+      setNotes(state);
     },
     removeNote: (state, action) => {
-      return state.filter(note => note.id !== action.payload);
+      const filterNotes = state.filter(note => note.id !== action.payload);
+      setNotes(filterNotes);
+      return filterNotes;
     },
     updateNote: (state, action) => {
       const { id, ...note } = action.payload;
@@ -99,6 +23,7 @@ const notesSlice = createSlice({
       if (existingNote) {
         Object.assign(existingNote, note);
       }
+      setNotes(state);
     }
   }
 });
