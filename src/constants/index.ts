e06325e1da89138e0110e312acca21398d48b0ec
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-10 19:56:16 +0800

    fix: add local storage keys for folder and trash

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,3 +4,4 @@ export const CONFIRM_DELETE_NOTE_MODAL_ID = 'confirm-delete-note-modal';
 export const CONFIRM_DELETE_FOLDER_MODAL_ID = 'confirm-delete-folder-modal';
 export const LOCAL_STORAGE_NOTES_KEY = 'notes';
 export const LOCAL_STORAGE_FOLDER_KEY = 'folders';
+export const LOCAL_STORAGE_TRASH_KEY = 'trash';
