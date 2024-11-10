26d6e247781150b73ff7407a42e20fd4458d72ac
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-10 19:54:35 +0800

    feat: add filter notes and reset filter reducer function

--- src/store/notesSlice.ts
+++ src/store/notesSlice.ts
@@ -4,30 +4,39 @@ import { getNotes, setNotes } from "../database/localstorage";
 const initialState: Array<Note> = getNotes();
 
 const notesSlice = createSlice({
-  name: 'notes',
+  name: "notes",
   initialState,
   reducers: {
     addNote: (state, action) => {
-
       state.push(action.payload);
       setNotes(state);
     },
     removeNote: (state, action) => {
-      const filterNotes = state.filter(note => note.id !== action.payload);
+      const filterNotes = state.filter((note) => note.id !== action.payload);
       setNotes(filterNotes);
       return filterNotes;
     },
     updateNote: (state, action) => {
       const { id, ...note } = action.payload;
-      const existingNote = state.find(note => note.id === id);
+      const existingNote = state.find((note) => note.id === id);
       if (existingNote) {
         Object.assign(existingNote, note);
       }
       setNotes(state);
-    }
-  }
+    },
+    fitlerNotes: (state, action) => {
+      return state.filter(
+        (note) => (note.title.includes(action.payload) ||
+          note.content.includes(action.payload) ||
+          note.tag?.text.includes(action.payload)),
+      );
+    },
+    resetFilter: () => {
+      return getNotes();
+    },
+  },
 });
 
-export const { addNote, removeNote, updateNote } = notesSlice.actions;
+export const { addNote, removeNote, updateNote, fitlerNotes, resetFilter } = notesSlice.actions;
 
 export default notesSlice.reducer;
