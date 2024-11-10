139f446187e172042a7970e82739945e2d17f36e
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-10 15:02:11 +0800

    refactor: rename modal constant names

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,6 @@
 export const PORTAL_ROOT_ID = 'portal-root';
 export const NOTE_MODAL_ID = 'note-modal';
-export const CONFIRM_MODAL_ID = 'confirm-modal';
+export const CONFIRM_DELETE_NOTE_MODAL_ID = 'confirm-delete-note-modal';
+export const CONFIRM_DELETE_FOLDER_MODAL_ID = 'confirm-delete-folder-modal';
+export const LOCAL_STORAGE_NOTES_KEY = 'notes';
+export const LOCAL_STORAGE_FOLDER_KEY = 'folders';
