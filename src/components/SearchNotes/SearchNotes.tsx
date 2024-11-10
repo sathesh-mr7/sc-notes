4a5203c968bc67e771af2431b005952fd4e06f88
sathesh Kumar <sathesh.mr7@gmail.com>
2024-11-10 20:04:45 +0800

    feat: add search functionality with debounce technic for search notes

--- src/components/SearchNotes/SearchNotes.tsx
+++ src/components/SearchNotes/SearchNotes.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { ReactComponent as SearchIcon } from "../../assets/icons/search.svg";
-import styles from "./SearchNotes.module.scss";
 import Input from "../Input/Input";
 
+import styles from "./SearchNotes.module.scss";
+import { debounce } from "../../utils/helper";
 interface SearchNotesProps {
   onChange: (text: string) => void;
 }
@@ -10,10 +11,18 @@ interface SearchNotesProps {
 const SearchNotes: React.FC<SearchNotesProps> = ({
   onChange
 }) => {
+  const [searchText, setSearchText] = useState("");
+  const debounceTimeout = 1000; 
+  const debouncedOnChange = useCallback(debounce(onChange, debounceTimeout), [onChange]);
+
+  const handleOnSearch = (text: string) => {
+    setSearchText(text);
+    debouncedOnChange(text);
+  }
   return (
     <div className={styles.container}>
       <SearchIcon className={styles.searchIcon} />
-      <Input onChange={(e) => onChange(e.target.value)} placeholder="Search notes" value="" className={styles.searchInput} />
+      <Input onChange={(e) => handleOnSearch(e.target.value)} placeholder="Search notes" value={searchText} className={styles.searchInput} />
     </div>
   )
 };
